e41f63d22a7bd0e88351c12910a6da68140a28fb
louliying <louliying@chexiang.com>
2018-10-16 17:46:05 +0800

    update

--- meta.js
+++ meta.js
@@ -53,91 +53,18 @@ module.exports = {
       type: 'string',
       message: '作者：',
     },
-    build: {
-      when: 'isNotTest',
-      type: 'list',
-      message: 'Vue build',
-      choices: [
-        {
-          name: 'Runtime + Compiler: recommended for most users',
-          value: 'standalone',
-          short: 'standalone',
-        },
-        {
-          name:
-            'Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere',
-          value: 'runtime',
-          short: 'runtime',
-        },
-      ],
-    },
-    router: {
-      when: 'isNotTest',
-      type: 'confirm',
-      message: '是否安装vue-router?',
-    },
+
+
     lint: {
       when: 'isNotTest',
       type: 'confirm',
       message: '需要使用 ESLint 语法规则和代码风格来检查你的代码吗?',
-    },
-    vuex: {
-      when: 'isNotTest',
-      type: 'confirm',
-      message: '是否安装vuex吗?',
-    },
-    lintConfig: {
-      when: 'isNotTest && lint',
-      type: 'list',
-      message: '请选择eslint规则:',
-      choices: [
-        {
-          name: 'Standard (https://github.com/standard/standard)',
-          value: 'standard',
-          short: 'Standard',
-        },
-        {
-          name: 'Airbnb (https://github.com/airbnb/javascript)',
-          value: 'airbnb',
-          short: 'Airbnb',
-        },
-        {
-          name: 'none (configure it yourself)',
-          value: 'none',
-          short: 'none',
-        },
-      ],
-    },
-    autoInstall: {
-      when: 'isNotTest',
-      type: 'list',
-      message:
-        'Should we run `npm install` for you after the project has been created? (recommended)',
-      choices: [
-        {
-          name: 'Yes, use NPM',
-          value: 'npm',
-          short: 'npm',
-        },
-        {
-          name: 'Yes, use Yarn',
-          value: 'yarn',
-          short: 'yarn',
-        },
-        {
-          name: 'No, I will handle that myself',
-          value: false,
-          short: 'no',
-        },
-      ],
-    },
+    }
   },
   // 根据条件过滤文件
   filters: {
     '.eslintrc.js': 'lint',
-    '.eslintignore': 'lint',
-    'src/router/**/*': 'router',
-    'src/store/**/*': 'vuex',
+    '.eslintignore': 'lint'
   },
   // 模板渲染完成后的回调函数， 优先于completeMessage
   complete: function(data, { chalk }) {
